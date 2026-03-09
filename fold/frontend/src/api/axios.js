cf4655c8b723d4c70edafc8d1a556a39c9e3f27b
raghavx2007-sudo <raghavx.2007@gmail.com>
2026-03-09 12:01:09 +0530

    Update axios.js
    routed to api in baseURL

--- fold/frontend/src/api/axios.js
+++ fold/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dualmind-ai-xubm.onrender.com", // change if needed
+  baseURL: "https://dualmind-ai-xubm.onrender.com/api", // change if needed
 });
 
 // Attach token automatically
