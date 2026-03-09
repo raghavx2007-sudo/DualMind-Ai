fda9259eb6f82e76990d510028a1c7d75e8a3b61
raghavx2007-sudo <raghavx.2007@gmail.com>
2026-03-09 07:55:29 +0000

    Frontend api call to hosted server

--- fold/frontend/src/api/axios.js
+++ fold/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dualmind-ai-xubm.onrender.com/api", // change if needed
+  baseURL: process.env.REACT_APP_API_URL, 
 });
 
 // Attach token automatically
